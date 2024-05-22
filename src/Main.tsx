37e8788e62d2f72f76d85cd4464f1de1fe4d5388
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-05-22 15:56:28 -0500

    initialized repo

---
+++ src/Main.tsx
@@ -0,0 +1,17 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+      <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </Router>,
+  </React.StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from "@vitejs/plugin-react"
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  build: {
+    sourcemap: true,
+    assetsDir: "code",
+    target: ["esnext"],
+    cssMinify: true,
+    lib: false
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: "injectManifest",
+      injectManifest: {
+        swSrc: 'public/sw.js',
+        swDest: 'dist/sw.js',
+        globDirectory: 'dist',
+        globPatterns: [
+          '**/*.{html,js,css,json,png}',
+        ],
+      },
+      injectRegister: false,
+      manifest: false,
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
