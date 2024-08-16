dd089b7fa236fb2d82960971b595ee6587523159
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-08-15 19:34:02 -0700

    updating... need to confirm PWA now

--- src/lib/db/supabase.ts
+++ src/lib/db/supabase.ts
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 // Don't forget types.
 export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-)
+  process.env.VITE_SUPABASE_URL!,
+  process.env.VITE_SUPABASE_ANON_KEY!
+);
+
